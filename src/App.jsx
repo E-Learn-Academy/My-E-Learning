8fbcfe2b19c60a4f3d7b011e4ac6d50757e142ef
Khaled Melgharib <khaledmelgharib@gmail.com>
2025-09-15 07:33:12 +0300

    feat: wrap application in AuthProvider and include Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Home from './Components/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './Components/auth/LoginPage';
 import RegisterPage from './Components/auth/RegisterPage';
+import { AuthProvider } from './Components/context/AuthenticationContext.jsx';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
-  
+      <AuthProvider>
+        <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path="/home" element={<Home />} />
             <Route path='/loginPage' element={<LoginPage/>}></Route>
       <Route path='/registerPage' element={<RegisterPage/>}></Route>
     </Routes>
-
+      </AuthProvider>
     </>
   )
 }
